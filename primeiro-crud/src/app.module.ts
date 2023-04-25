d26672684004f673c3937756f5f36cd8bca6d352
glcalil-ufg <glcalil@discente.ufg.br>
2023-04-25 00:08:51 -0300

    adicionado knex e conectado ao database

--- primeiro-crud/src/app.module.ts
+++ primeiro-crud/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { KnexModule } from 'nestjs-knex';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule,
+    KnexModule.forRoot({
+      config: {
+        client: "pg",
+        connection: {
+          host: 'localhost',
+          user: 'postgres',
+          password: 'asdf1234',
+          database: 'CRUD' 
+        }
+      },
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
