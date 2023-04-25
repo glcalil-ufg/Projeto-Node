a94a6f10f866ab4d8e17345df2a65bc6af250560
glcalil-ufg <glcalil@discente.ufg.br>
2023-04-24 22:16:25 -0300

    Criado projeto primeiro crud com documentação

---
+++ primeiro-crud/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle("CRUD With NestJs")
+  .setDescription("My first crud with nestjs")
+  .setVersion('1.0')
+  .addTag('user')
+  .addTag('start')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
